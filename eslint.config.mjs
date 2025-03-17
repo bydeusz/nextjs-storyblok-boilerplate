dd1ab79df81e48f2772a6c3701a4da14938c43d8
Tadeusz de Ruijter <46708285+bydeusz@users.noreply.github.com>
2025-03-17 10:09:56 +0100

    fixed eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierConfig from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.plugins("prettier", "jsx-a11y"),
+  prettierConfig,
+  {
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
